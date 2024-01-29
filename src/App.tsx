143916c63907bdcd9da3a0626b92fd4301464d93
Paul Verdure <paul.verdure@gmail.com>
2024-01-29 21:51:20 +0100

    feat: add a button to test getRecipesTitles

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,29 @@ import { SignInButton, UserButton } from "@clerk/clerk-react";
 import {
   Authenticated,
   Unauthenticated,
+  useAction,
   useMutation,
   useQuery,
 } from "convex/react";
 import { api } from "../convex/_generated/api";
+import { toNamespacedPath } from "path";
+import { useState } from "react";
 
 export default function App() {
-  const messages = useQuery(api.messages.list) || [];
-  const sendMessage = useMutation(api.messages.send);
+  //
+  const INGREDIENTS = [
+    "garlic",
+    "tomato",
+    "onion",
+    "zucchini",
+    "mozzarella",
+    "basil",
+    "olive oil",
+  ];
+  const [response, setResponse] = useState("");
+  const [loading, setLoading] = useState(false);
+  const getTitles = useAction(api.openai.getRecipesTitles);
+
   return (
     <main className="container max-w-2xl flex flex-col gap-8">
       <h1 className="text-4xl font-extrabold my-8 text-center">
@@ -26,67 +41,31 @@ export default function App() {
           </SignInButton>
         </div>
       </Unauthenticated>
+      <div className="flex justify-center">
+        <button
+          onClick={() => {
+            setLoading(true);
+            getTitles({ ingredients: INGREDIENTS })
+              .then((titles) => {
+                setLoading(false);
+                setResponse(titles);
+              })
+              .catch((error) => {
+                console.error(error);
+              });
+          }}
+        >
+          Get Recipes
+        </button>
+      </div>
     </main>
   );
 }
 
 function SignedIn() {
-  const { numbers, viewer } =
-    useQuery(api.myFunctions.listNumbers, {
-      count: 10,
-    }) ?? {};
-  const addNumber = useMutation(api.myFunctions.addNumber);
-
   return (
     <>
-      <p>Welcome {viewer}!</p>
-      <p className="flex gap-4 items-center">
-        This is you:
-        <UserButton afterSignOutUrl="#" />
-      </p>
-      <p>
-        Click the button below and open this page in another window - this data
-        is persisted in the Convex cloud database!
-      </p>
-      <p>
-        <Button
-          onClick={() => {
-            void addNumber({ value: Math.floor(Math.random() * 10) });
-          }}
-        >
-          Add a random number
-        </Button>
-      </p>
-      <p>
-        Numbers:{" "}
-        {numbers?.length === 0
-          ? "Click the button!"
-          : numbers?.join(", ") ?? "..."}
-      </p>
-      <p>
-        Edit{" "}
-        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
-          convex/myFunctions.ts
-        </code>{" "}
-        to change your backend
-      </p>
-      <p>
-        Edit{" "}
-        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
-          src/App.tsx
-        </code>{" "}
-        to change your frontend
-      </p>
-      <p>
-        Check out{" "}
-        <a
-          className="font-medium text-primary underline underline-offset-4"
-          target="_blank"
-          href="https://docs.convex.dev/home"
-        >
-          Convex docs
-        </a>
-      </p>
+      <p>Welcome!</p>
     </>
   );
 }
