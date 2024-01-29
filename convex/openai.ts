5f22882f9ec55c721e36f05f9e75db29639088f7
Paul Verdure <paul.verdure@gmail.com>
2024-01-29 21:49:31 +0100

    chore: add getRecipesTitles in openai.ts

---
+++ convex/openai.ts
@@ -0,0 +1,30 @@
+"use node";
+import OpenAI from "openai";
+import { v } from "convex/values";
+import { action } from "./_generated/server";
+
+export const getRecipesTitles = action({
+  args: { ingredients: v.array(v.string()) },
+  handler: async (ctx, { ingredients }) => {
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    const prompt =
+      "Someone is opening its fridge and kitchen cupboard. They find the following ingredients: " +
+      ingredients.join(", ") +
+      ". They want to cook a main dish with these ingredients. What should they cook. Provide them with 10 different recipes titles.";
+    console.log("prompt", prompt);
+
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      model: "gpt-3.5-turbo",
+    });
+    console.log(chatCompletion.choices[0].message);
+    return chatCompletion.choices[0].message.content;
+  },
+});
