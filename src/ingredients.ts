ce4dd2c7df6f641d14a3d67bfdb1ebd4ff30725e
Paul Verdure <paul.verdure@gmail.com>
2024-01-30 21:51:47 +0100

    feat: add a first list of ingredients options

---
+++ src/ingredients.ts
@@ -0,0 +1,100 @@
+import { IngredientsOptions } from "./types";
+
+export const ingredients: IngredientsOptions = [
+  {
+    id: 1,
+    name: "Tomato: 🍅"
+  },
+  {
+    id: 2,
+    name: "Cheese: 🧀"
+  },
+  {
+    id: 3,
+    name: "Salami: 🍖"
+  },
+  {
+    id: 4,
+    name: "Ham: 🍖"
+  },
+  {
+    id: 5,
+    name: "Mushrooms: 🍄"
+  },
+  {
+    id: 6,
+    name: "Pineapple: 🍍"
+  },
+  {
+    id: 7,
+    name: "Pepperoni: 🍕"
+  },
+  {
+    id: 8,
+    name: "Onion: 🧅"
+  },
+  {
+    id: 9,
+    name: "Pepper: 🌶️"
+  },
+  {
+    id: 10,
+    name: "Olive: 🫒"
+  },
+  {
+    id: 11,
+    name: "Bacon: 🥓"
+  },
+  {
+    id: 12,
+    name: "Corn: 🌽"
+  },
+  {
+    id: 13,
+    name: "Spinach: 🥬"
+  },
+  {
+    id: 14,
+    name: "Egg: 🥚"
+  },
+  {
+    id: 15,
+    name: "Tuna: 🐟"
+  },
+  {
+    id: 16,
+    name: "Shrimp: 🍤"
+  },
+  {
+    id: 17,
+    name: "Anchovy: 🐟"
+  },
+  {
+    id: 18,
+    name: "Garlic: 🧄"
+  },
+  {
+    id: 19,
+    name: "Chili: 🌶️"
+  },
+  {
+    id: 20,
+    name: "Paprika: 🌶️"
+  },
+  {
+    id: 21,
+    name: "Sausage: 🌭"
+  },
+  {
+    id: 22,
+    name: "Chicken: 🍗"
+  },
+  {
+    id: 23,
+    name: "Beef: 🥩"
+  },
+  {
+    id: 24,
+    name: "Pork: 🍖"
+  },
+];
