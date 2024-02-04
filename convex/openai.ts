14b0c3bf605a5e65aa129488599cbd28790d31b5
Paul Verdure <paul.verdure@gmail.com>
2024-02-04 20:27:33 +0100

    fix: typo in openai.ts

--- convex/openai.ts
+++ convex/openai.ts
@@ -26,6 +26,6 @@ export const getRecipesTitles = action({
       model: "gpt-3.5-turbo",
     });
     console.log(chatCompletion.choices[0].message);
-    return [chatCompletion?.choices[0]?.message?.content].filter(Boolean);
+    return chatCompletion?.choices[0]?.message?.content;
   },
 });
