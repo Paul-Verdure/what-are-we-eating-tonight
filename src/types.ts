7053a5e4d3acc955c0a9cd715fa0ca01c9f7d3c2
Paul Verdure <paul.verdure@gmail.com>
2024-02-05 21:27:22 +0100

    type: add Recipe types

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,10 @@ export type IngredientsOptions = {
   id: number;
   name: string;
 }[];
+
+export type Recipe = {
+  recipe: string;
+  ingredients: string[];
+};
+
+export type RecipeOptions = Recipe[];
