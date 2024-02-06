193ec93bf56641845bb402be9fcc59b1c6379532
Paul Verdure <paul.verdure@gmail.com>
2024-02-06 21:00:14 +0100

    fix: remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { SignInButton } from "@clerk/clerk-react";
-import { Authenticated, Unauthenticated, useAction } from "convex/react";
-import { api } from "../convex/_generated/api";
-import { useState } from "react";
-import { RecipeOptions } from "./types";
+import { Authenticated, Unauthenticated } from "convex/react";
 import RecipeFinder from "./RecipeFinder";
 
 export default function App() {
