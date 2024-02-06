7b825f6c5de62ec0e541940cb7106e4445eaaadb
Paul Verdure <paul.verdure@gmail.com>
2024-02-06 17:31:48 +0100

    feat: taking out data fetching form App

--- src/App.tsx
+++ src/App.tsx
@@ -3,49 +3,16 @@ import { SignInButton } from "@clerk/clerk-react";
 import { Authenticated, Unauthenticated, useAction } from "convex/react";
 import { api } from "../convex/_generated/api";
 import { useState } from "react";
-import { get } from "http";
 import { RecipeOptions } from "./types";
+import RecipeFinder from "./RecipeFinder";
 
 export default function App() {
   //
-  const INGREDIENTS = [
-    "garlic",
-    "tomato",
-    "onion",
-    "zucchini",
-    "mozzarella",
-    "basil",
-    "olive oil",
-  ];
-  const [response, setResponse] = useState<RecipeOptions>([]);
-  const [loading, setLoading] = useState(false);
-  const getTitles = useAction(api.openai.getRecipesTitles);
-
-  console.log(response);
-
-  function handleClick() {
-    setLoading(true);
-    getTitles({ ingredients: INGREDIENTS })
-      .then((result) => {
-        console.log(result);
-        if (result) {
-          setResponse(JSON.parse(result));
-          setLoading(false);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        setLoading(false);
-      });
-  }
 
   return (
-    <main className="container max-w-2xl flex flex-col gap-8">
-      <h1 className="text-4xl font-extrabold my-8 text-center">
-        Convex + React (Vite) + Clerk Auth
-      </h1>
+    <main className="container max-w-6xl flex flex-col gap-8">
       <Authenticated>
-        <SignedIn />
+        <RecipeFinder />
       </Authenticated>
       <Unauthenticated>
         <div className="flex justify-center">
@@ -54,28 +21,7 @@ export default function App() {
           </SignInButton>
         </div>
       </Unauthenticated>
-      <div className="flex justify-center">
-        <button onClick={handleClick}>Get Recipes</button>
-        {/* <section>
-          {loading ? (
-            <p>Loading...</p>
-          ) : (
-            <div>
-              {response.map((title, index) => {
-                return <p key={index}>{title}</p>;
-              })}
-            </div>
-          )}
-        </section> */}
-      </div>
     </main>
   );
 }
 
-function SignedIn() {
-  return (
-    <>
-      <p>Welcome!</p>
-    </>
-  );
-}
