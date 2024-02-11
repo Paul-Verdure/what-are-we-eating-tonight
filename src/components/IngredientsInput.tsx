6e2a5fc2cd8203b81f08731c5b711e91365b23b8
Paul Verdure <paul.verdure@gmail.com>
2024-02-11 14:28:17 +0100

    feat: add a series of checkbox to select food preferences

--- src/components/IngredientsInput.tsx
+++ src/components/IngredientsInput.tsx
@@ -9,16 +9,22 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
+  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Dispatch } from "react";
+import { Checkbox } from "@radix-ui/react-checkbox";
 
 const FormSchema = z.object({
   ingredient: z.string().min(2, {
     message: "Ingredient must be at least 2 characters.",
+  }),
+  items: z.array(z.string()).refine((value) => value.some((item) => item), {
+    message: "You have to select at least one item.",
   }),
 });
 
@@ -29,10 +35,38 @@ type IngredientsInputProps = {
 export function IngredientsInput({
   setSelectedIngredients,
 }: IngredientsInputProps) {
+  const items = [
+    {
+      id: "recents",
+      label: "Recents",
+    },
+    {
+      id: "home",
+      label: "Home",
+    },
+    {
+      id: "applications",
+      label: "Applications",
+    },
+    {
+      id: "desktop",
+      label: "Desktop",
+    },
+    {
+      id: "downloads",
+      label: "Downloads",
+    },
+    {
+      id: "documents",
+      label: "Documents",
+    },
+  ] as const;
+
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
       ingredient: "",
+      items: [],
     },
   });
 
@@ -54,6 +88,55 @@ export function IngredientsInput({
               <FormControl>
                 <Input placeholder="Enter an ingredient" {...field} />
               </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="items"
+          render={() => (
+            <FormItem>
+              <div className="mb-4">
+                <FormLabel className="text-base">Sidebar</FormLabel>
+                <FormDescription>
+                  Select the items you want to display in the sidebar.
+                </FormDescription>
+              </div>
+              {items.map((item) => (
+                <FormField
+                  key={item.id}
+                  control={form.control}
+                  name="items"
+                  render={({ field }) => {
+                    return (
+                      <FormItem
+                        key={item.id}
+                        className="flex flex-row items-start space-x-3 space-y-0"
+                      >
+                        <FormControl>
+                          <Checkbox
+                            checked={field.value?.includes(item.id)}
+                            onCheckedChange={(checked) => {
+                              return checked
+                                ? field.onChange([...field.value, item.id])
+                                : field.onChange(
+                                    field.value?.filter(
+                                      (value) => value !== item.id
+                                    )
+                                  );
+                            }}
+                          />
+                        </FormControl>
+                        <FormLabel className="text-sm font-normal">
+                          {item.label}
+                        </FormLabel>
+                      </FormItem>
+                    );
+                  }}
+                />
+              ))}
               <FormMessage />
             </FormItem>
           )}
