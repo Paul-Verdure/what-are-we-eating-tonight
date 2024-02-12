ed5708ed8c81dabd475ff07776e4682295703218
Paul Verdure <paul.verdure@gmail.com>
2024-02-12 18:18:12 +0100

    feat: create checkbox list for food preferences

---
+++ src/components/FinderInputs/checkboxLists.ts
@@ -0,0 +1,40 @@
+import { CheckboxItems } from "@/types";
+
+export const foodPreferences: CheckboxItems = [
+  {
+    id: "vegetarian",
+    label: "Vegetarian",
+  },
+  {
+    id: "vegan",
+    label: "Vegan",
+  },
+  {
+    id: "gluten-free",
+    label: "Gluten-free",
+  },
+  {
+    id: "dairy-free",
+    label: "Dairy-free",
+  },
+  {
+    id: "nut-free",
+    label: "Nut-free",
+  },
+  {
+    id: "egg-free",
+    label: "Egg-free",
+  },
+  {
+    id: "seafood-free",
+    label: "Seafood-free",
+  },
+  {
+    id: "soy-free",
+    label: "Soy-free",
+  },
+  {
+    id: "pork-free",
+    label: "Pork-free",
+  },
+] as const;
