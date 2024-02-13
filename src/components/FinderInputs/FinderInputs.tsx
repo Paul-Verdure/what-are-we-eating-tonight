f4e9c2baac95505d72e4fb174824a669e6f49899
Paul Verdure <paul.verdure@gmail.com>
2024-02-13 20:49:53 +0100

    fix: remove checkboxes from form

--- src/components/FinderInputs/FinderInputs.tsx
+++ src/components/FinderInputs/FinderInputs.tsx
@@ -9,54 +9,34 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Dispatch } from "react";
-import { Checkbox } from "@/components/ui/checkbox";
-import { foodPreferences, mealChoices } from "./checkboxLists";
 
 const FormSchema = z.object({
   ingredient: z.string().min(2, {
     message: "Ingredient must be at least 2 characters.",
   }),
-  foodPreferences: z.array(z.string()),
-  mealChoices: z
-    .array(z.string())
-    .refine((value) => value.some((item) => item), {
-      message: "You have to select at least one item.",
-    }),
 });
 
 type FinderInputsProps = {
   setSelectedIngredients: Dispatch<React.SetStateAction<string[]>>;
-  setSelectedPreferences: Dispatch<React.SetStateAction<string[]>>;
-  setSelectedMeal: Dispatch<React.SetStateAction<string | undefined>>;
 };
 
-export function FinderInputs({
-  setSelectedIngredients,
-  setSelectedPreferences,
-  setSelectedMeal,
-}: FinderInputsProps) {
+export function FinderInputs({ setSelectedIngredients }: FinderInputsProps) {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
       ingredient: "",
-      foodPreferences: [],
-      mealChoices: [],
     },
   });
 
   const onSubmit = (data: z.infer<typeof FormSchema>) => {
     console.log(data);
     setSelectedIngredients((prev: string[]) => [...prev, data.ingredient]);
-    setSelectedPreferences(data.foodPreferences);
-    setSelectedMeal(data.mealChoices[0]);
     form.reset();
   };
 
@@ -71,97 +51,6 @@ export function FinderInputs({
               <FormControl>
                 <Input placeholder="Enter an ingredient" {...field} />
               </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="foodPreferences"
-          render={() => (
-            <FormItem>
-              <div className="mb-4">
-                <FormDescription>Select your food preferences</FormDescription>
-              </div>
-              {foodPreferences.map((item) => (
-                <FormField
-                  key={item.id}
-                  control={form.control}
-                  name="foodPreferences"
-                  render={({ field }) => {
-                    return (
-                      <FormItem
-                        key={item.id}
-                        className="flex flex-row items-start space-x-3 space-y-0"
-                      >
-                        <FormControl>
-                          <Checkbox
-                            checked={field.value?.includes(item.id)}
-                            onCheckedChange={(checked) => {
-                              return checked
-                                ? field.onChange([...field.value, item.id])
-                                : field.onChange(
-                                    field.value?.filter(
-                                      (value) => value !== item.id
-                                    )
-                                  );
-                            }}
-                          />
-                        </FormControl>
-                        <FormLabel className="text-sm font-normal">
-                          {item.label}
-                        </FormLabel>
-                      </FormItem>
-                    );
-                  }}
-                />
-              ))}
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="foodPreferences"
-          render={() => (
-            <FormItem>
-              <div className="mb-4">
-                <FormDescription>Select a meal</FormDescription>
-              </div>
-              {mealChoices.map((item) => (
-                <FormField
-                  key={item.id}
-                  control={form.control}
-                  name="mealChoices"
-                  render={({ field }) => {
-                    return (
-                      <FormItem
-                        key={item.id}
-                        className="flex flex-row items-start space-x-3 space-y-0"
-                      >
-                        <FormControl>
-                          <Checkbox
-                            checked={field.value?.includes(item.id)}
-                            onCheckedChange={(checked) => {
-                              return checked
-                                ? field.onChange([...field.value, item.id])
-                                : field.onChange(
-                                    field.value?.filter(
-                                      (value) => value !== item.id
-                                    )
-                                  );
-                            }}
-                          />
-                        </FormControl>
-                        <FormLabel className="text-sm font-normal">
-                          {item.label}
-                        </FormLabel>
-                      </FormItem>
-                    );
-                  }}
-                />
-              ))}
               <FormMessage />
             </FormItem>
           )}
