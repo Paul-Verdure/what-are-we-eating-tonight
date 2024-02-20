01770b1711ea279490612f48861b3a08b73eb841
Paul Verdure <paul.verdure@gmail.com>
2024-02-20 08:28:50 +0100

    feat: change getRecipeTitle to filter per food preferences

--- convex/openai.ts
+++ convex/openai.ts
@@ -4,51 +4,47 @@ import { v } from "convex/values";
 import { action } from "./_generated/server";
 
 export const getRecipesTitles = action({
-  args: { ingredients: v.array(v.string()) },
-  handler: async (ctx, { ingredients }) => {
+  args: { ingredients: v.array(v.string()), preferences: v.array(v.string()) },
+  handler: async (ctx, { ingredients, preferences }) => {
     const openai = new OpenAI({
       apiKey: process.env.OPENAI_API_KEY,
     });
 
-    const prompt = `Create a list of 8 recipes that can be made with the following ingredients: ${ingredients.join(
+    const prompt = `Create a list of up to 8 ${preferences.join(
       ", "
-    )}.
-    Do not include any explanations, only provide a  RFC8259 compliant JSON response  following this format without deviation.
+    )} recipes using the ingredients: ${ingredients.join(
+      ", "
+    )}. You can choose not to use all listed ingredients in each recipe, and you may include up to 3 additional ingredients of your own in each recipe.
+    
+    Provide an RFC8259 compliant JSON response without explanations:
+    
     [{
-        "title": "Recipe Title 1",
-        "description": "Recipe description 1",
-      }, {
-        "title": "Recipe Title 2",
-        "description": "Recipe description 2",
+      "title": "Recipe Title 1",
+      "description": "Recipe description 1"
+    }, {
+      "title": "Recipe Title 2",
+      "description": "Recipe description 2"
+    }, {
+      "title": "Recipe Title 3",
+      "description": "Recipe description 3"
+    }, {
+      "title": "Recipe Title 4",
+      "description": "Recipe description 4"
+    }, {
+      "title": "Recipe Title 5",
+      "description": "Recipe description 5"
+    }, {
+      "title": "Recipe Title 6",
+      "description": "Recipe description 6"
+    }, {
+      "title": "Recipe Title 7",
+      "description": "Recipe description 7"
+    }, {
+      "title": "Recipe Title 8",
+      "description": "Recipe description 8"
+    }]`;
 
-      }, {
-        "title": "Recipe Title 3",
-        "description": "Recipe description 3",
-
-      }, {
-        "title": "Recipe Title 4",
-        "description": "Recipe description ",
-
-      }, {
-        "title": "Recipe Title 5",
-        "description": "Recipe description 5",
-
-      }, {
-        "title": "Recipe Title 6",
-        "description": "Recipe description 6",
-
-      }, {
-        "title": "Recipe Title 7",
-        "description": "Recipe description 7",
-
-      }, {
-        "title": "Recipe Title 8",
-        "description": "Recipe description 8",
-
-      }]
-    `;
-
-
+    console.log("prompt", prompt);
 
     const chatCompletion = await openai.chat.completions.create({
       model: "gpt-3.5-turbo-0613",
