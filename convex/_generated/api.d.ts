a25cb76130079545d2f0b9216e7df7c8505956c8
Paul Verdure <paul.verdure@gmail.com>
2024-05-29 22:45:58 +0200

    feat: create useCurrentUser hook

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as favoriteRecipes from "../favoriteRecipes.js";
 import type * as http from "../http.js";
 import type * as openai from "../openai.js";
 import type * as users from "../users.js";
@@ -27,6 +28,7 @@ import type * as users from "../users.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  favoriteRecipes: typeof favoriteRecipes;
   http: typeof http;
   openai: typeof openai;
   users: typeof users;
