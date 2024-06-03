6815d396f3c07763981a81b9d21c3c47cccc6939
Paul Verdure <paul.verdure@gmail.com>
2024-06-03 13:46:23 +0200

    feat: add message if no saved recipe

--- src/MyRecipes.tsx
+++ src/MyRecipes.tsx
@@ -14,21 +14,37 @@ export default function MyRecipes() {
         <h1 className="text-3xl font-bold tracking-tighter">
           My favorite recipes
         </h1>
-        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4">
-          {favoriteRecipes?.map((recipe) => (
-            <Card key={recipe?._id}>
-              <CardContent className="p-4 flex flex-col h-full justify-evenly gap-3">
-                <h3 className="font-semibold tracking-tight">
-                  {recipe?.title}
-                </h3>
-                <p className="text-sm leading-none text-gray-500 dark:text-gray-400">
-                  {recipe?.description}
-                </p>
-                <FavoriteRecipeDetailsDialog recipe={recipe} />
-              </CardContent>
-            </Card>
-          ))}
-        </div>
+        {favoriteRecipes ? (
+          <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4">
+            {favoriteRecipes.map((recipe) => (
+              <Card key={recipe?._id}>
+                <CardContent className="p-4 flex flex-col h-full justify-evenly gap-3">
+                  <h3 className="font-semibold tracking-tight">
+                    {recipe?.title}
+                  </h3>
+                  <p className="text-sm leading-none text-gray-500 dark:text-gray-400">
+                    {recipe?.description}
+                  </p>
+                  <FavoriteRecipeDetailsDialog recipe={recipe} />
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        ) : (
+          <div className="animate-pulse container">
+            <h3 className="font-semibold tracking-tight text-center">
+              Loading your favorite recipes...
+            </h3>
+          </div>
+        )}
+
+        {favoriteRecipes?.length === 0 && (
+          <div className="container">
+            <h3 className="font-semibold tracking-tight text-center">
+              You haven't saved any recipes yet.
+            </h3>
+          </div>
+        )}
       </div>
     </section>
   );
