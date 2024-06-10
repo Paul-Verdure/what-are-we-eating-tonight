489f0ea8f76f21be1facd5191c14e63eb2c57656
Paul Verdure <paul.verdure@gmail.com>
2024-06-10 11:47:35 +0200

    feat: change layout in My Recipes

--- src/MyRecipes.tsx
+++ src/MyRecipes.tsx
@@ -1,51 +1,53 @@
-import { api } from "../convex/_generated/api";
-import { useQuery } from "convex/react";
-import { Card, CardContent } from "./components/ui/card";
-import { FavoriteRecipeDetailsDialog } from "./components/FavoriteRecipeDetailsDialog/FavoriteRecipeDetailsDialog";
+import { api } from '../convex/_generated/api'
+import { useQuery } from 'convex/react'
+import { Card, CardContent } from './components/ui/card'
+import { FavoriteRecipeDetailsDialog } from './components/FavoriteRecipeDetailsDialog/FavoriteRecipeDetailsDialog'
 
 export default function MyRecipes() {
-  const favoriteRecipes = useQuery(api.favoriteRecipes.getFavoriteRecipes);
-
-  console.log("favoriteRecipes", favoriteRecipes);
+  const favoriteRecipes = useQuery(api.favoriteRecipes.getFavoriteRecipes)
 
   return (
-    <section className="container px-4 md:px-6">
-      <div className="flex flex-col gap-6">
-        <h1 className="text-3xl font-bold tracking-tighter">
-          My favorite recipes
-        </h1>
-        {favoriteRecipes ? (
-          <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4">
-            {favoriteRecipes.map((recipe) => (
-              <Card key={recipe?._id}>
-                <CardContent className="p-4 flex flex-col h-full justify-evenly gap-3">
-                  <h3 className="font-semibold tracking-tight">
-                    {recipe?.title}
-                  </h3>
-                  <p className="text-sm leading-none text-gray-500 dark:text-gray-400">
-                    {recipe?.description}
-                  </p>
-                  <FavoriteRecipeDetailsDialog recipe={recipe} />
-                </CardContent>
-              </Card>
-            ))}
-          </div>
-        ) : (
-          <div className="animate-pulse container">
-            <h3 className="font-semibold tracking-tight text-center">
-              Loading your favorite recipes...
-            </h3>
-          </div>
-        )}
+    <section className="text-center md:mb-12">
+      <h1 className="font-display text-3xl font-bold tracking-tight md:text-5xl">
+        My favorite recipes
+      </h1>
+      <p className="mt-4 text-gray-500 dark:text-gray-400">
+        Here are the recipes you saved. Enjoy! 😋
+      </p>
+      {favoriteRecipes ? (
+        <div className="mt-12 grid place-content-center gap-6 md:grid-cols-2 lg:grid-cols-4">
+          {favoriteRecipes.map((recipe) => (
+            <Card
+              key={recipe?._id}
+              className="max-w-sm overflow-hidden rounded shadow-lg"
+            >
+              <CardContent className="flex h-full flex-col justify-between gap-8 px-4 py-8">
+                <h3 className="text-lg font-semibold tracking-tight">
+                  {recipe?.title}
+                </h3>
+                <p className="text-sm leading-none text-gray-500 dark:text-gray-400">
+                  {recipe?.description}
+                </p>
+                <FavoriteRecipeDetailsDialog recipe={recipe} />
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      ) : (
+        <div className="container animate-pulse mt-12">
+          <h3 className="text-center font-semibold tracking-tight">
+            Loading your favorite recipes... 😋
+          </h3>
+        </div>
+      )}
 
-        {favoriteRecipes?.length === 0 && (
-          <div className="container">
-            <h3 className="font-semibold tracking-tight text-center">
-              You haven't saved any recipes yet.
-            </h3>
-          </div>
-        )}
-      </div>
+      {favoriteRecipes?.length === 0 && (
+        <div className="container">
+          <h3 className="text-center font-semibold tracking-tight">
+            You haven't saved any recipes yet.
+          </h3>
+        </div>
+      )}
     </section>
-  );
+  )
 }
